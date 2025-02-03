a12b713d9174fb62ed90b9441c7388a20152f7bf
Krishna Sai Kannekanti <krishna@tarafirst.com>
2025-02-03 19:14:57 +0530

    adding corporate dashboard

--- src/views/admin/dashboard/corporate.jsx
+++ src/views/admin/dashboard/corporate.jsx
@@ -1,16 +1,17 @@
 'use client';
-import Grid from '@mui/material/Grid2';
-
 import Factory from '@/utils/Factory';
 import { useState, useEffect } from 'react';
 
 // @project
 // import Services from './visaconsultencyFiles/Services';
 import { useSnackbar } from '@/components/CustomSnackbar';
-import OverviewCard from './corporateAdminComponenets/OverviewCard';
+import OverviewCard from './corporate-admin-componenets/OverviewCard';
 import { useAuth } from '@/contexts/AuthContext';
-import { Stack, Typography } from '@mui/material';
-/***************************  ANALYTICS - OVERVIEW  ***************************/
+import UserBehaviourTable from '@/sections/dashboard/analytics/user-behavior';
+import Grid from '@mui/material/Grid2';
+import MainCard from '@/components/MainCard';
+import { Box, Chip, Stack, Typography } from '@mui/material';
+import ZombieingDoodle from '@/images/illustration/ZombieingDoodle';
 
 export default function CorporateEntity() {
   //   const { user } = useAuth();
@@ -19,39 +20,86 @@ export default function CorporateEntity() {
   const { showSnackbar } = useSnackbar();
   const { user, tokens, logout } = useAuth();
 
-  const getClientsData = async () => {
-    const url = '/user_management/visa-clients/dashboard-status/';
-    try {
-      const { res, error } = await Factory('get', url, {});
-      if (res.status_cd === 0) {
-        setClientListData(res.data);
-      }
-    } catch (error) {
-      // Catch any errors during the request
-      console.error('Error:', error);
-      showSnackbar(JSON.stringify(error), 'error');
-    }
-  };
+  // const getClientsData = async () => {
+  //   const url = '/user_management/visa-clients/dashboard-status/';
+  //   try {
+  //     const { res, error } = await Factory('get', url, {});
+  //     if (res.status_cd === 0) {
+  //       setClientListData(res.data);
+  //     }
+  //   } catch (error) {
+  //     // Catch any errors during the request
+  //     console.error('Error:', error);
+  //     showSnackbar(JSON.stringify(error), 'error');
+  //   }
+  // };
 
-  useEffect(() => {
-    getClientsData(); // Load client list on component mount
-  }, []);
+  // useEffect(() => {
+  //   getClientsData(); // Load client list on component mount
+  // }, []);
 
   return (
-    <Grid container spacing={{ xs: 2, md: 3 }}>
-      <Stack sx={{ gap: 0 }}>
-        <Typography variant="h4" sx={{ fontWeight: 400 }}>
-          Corporate Admin
-        </Typography>
-        <Typography variant="caption" sx={{ color: 'grey.700' }}>
-          View or create your services
-        </Typography>
-      </Stack>
-      <Grid size={12}>
-        {' '}
-        <OverviewCard clientListData={clientListData} />{' '}
+    <Box>
+      <Grid container sx={{ mb: 3 }} spacing={{ xs: 2, md: 3 }}>
+        <Grid container size={12}>
+          <Grid size={{ xs: 12, md: 8 }}>
+            <MainCard style={{ display: 'flex', flexDirection: 'row' }}>
+              <Grid
+                size={8}
+                style={{
+                  display: 'flex',
+                  flexDirection: 'column',
+                  justifyContent: 'center'
+                }}
+              >
+                <Typography variant="h3">Hello, User!</Typography>
+                <Typography variant="h5">Welcome back to Tara First</Typography>
+                <Typography color="grey.600" sx={{ mt: 2 }} variant="subtitle1">
+                  Tara gives you the power to handle taxes, incorporate your business, stay compliant, run payroll, and create invoices –
+                  all in a few clicks. Let us help you focus on your wellbeing while we take care of the rest!
+                </Typography>
+              </Grid>
+              <Grid size={4}>
+                <ZombieingDoodle />
+              </Grid>
+            </MainCard>
+          </Grid>
+          <Grid size={{ xs: 12, md: 4 }}>
+            <MainCard style={{ minHeight: '100%', maxHeight: '100%' }}>
+              <Stack sx={{ gap: 1.5 }}>
+                <Typography variant="subtitle1">
+                  <Stack direction={'column'} sx={{ gap: 0 }}>
+                    User Since Dec, 2023{' '}
+                    <Typography variant="subtitle2" color="grey.700">
+                      Last Login - Jan 30, 2025
+                    </Typography>
+                  </Stack>
+                </Typography>
+                <Typography variant="subtitle1">
+                  <Stack direction={'row'} sx={{ gap: 1 }}>
+                    User KYC Status - <Typography color="success">Completed</Typography>
+                  </Stack>
+                </Typography>
+                <Typography variant="subtitle1">
+                  <Stack direction={'row'} sx={{ gap: 1 }}>
+                    Subscription Plan - <Typography color="primary">Premium</Typography>
+                  </Stack>
+                </Typography>
+
+                <Typography variant="subtitle1">Next Renewal - March 10, 2025</Typography>
+              </Stack>
+            </MainCard>
+          </Grid>
+        </Grid>
       </Grid>
-      <Grid size={12}>{/* <Services /> */}</Grid>
-    </Grid>
+
+      <UserBehaviourTable />
+
+      <Grid container sx={{ mt: 3 }} spacing={{ xs: 2, md: 3 }}>
+        <Grid size={12}>
+          <OverviewCard clientListData={clientListData} />{' '}
+        </Grid>
+      </Grid>
+    </Box>
   );
 }
